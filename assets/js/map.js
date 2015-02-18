a9bb0ebeb73f5227e03dd42fbc22807846b90210
danielviridislearning <daniel@viridislearning.com>
2015-02-18 18:22:27 -0300

    cambios en las rutas de map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,15 @@
 var map;
 
+var Markers_Config = {
+    atm_markers: "",
+    atm_markers_lenght: "",
 
+    branch_markers: "",
+    branch_markers_lenght: "",
+
+    tas_markers: "",
+    tas_markers_lenght: ""
+}
 
 var pos;
 var userMarker = null;
@@ -12,11 +21,11 @@ var directionsService = new google.maps.DirectionsService();
 var markersATMS = [];
 var markersBranches = [];
 var markersActive = '000';
-//Se coloca de manera relativa las carpetas
-var bundle_prefix = '/fbwebmap/assets/';
 
-var atm_image = bundle_prefix + '/img/atm.png';
-var branch_image = bundle_prefix + '/img/branch.png';
+var bundle_prefix = '/assets/';
+
+var atm_image = bundle_prefix + 'img/atm.png';
+var branch_image = bundle_prefix + 'img/branch.png';
 
 String.prototype.replaceAt = function(index, character) {
     return this.substr(0, index) + character + this.substr(index + character.length);
